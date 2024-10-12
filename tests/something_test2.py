08ef7ff093cb48804f456a6000d9aa36eff3dbe7
Boris <borisencodg@gmail.com>
2024-10-12 16:05:31 +0300

    solveme lesson 4.1

--- tests/something_test2.py
+++ tests/something_test2.py
@@ -6,7 +6,6 @@ from configuration import SERVICE_URL2
 
 #resp = requests.get(SERVICE_URL2)
 
-def test_get_user_list():
-    response = requests.get(SERVICE_URL2)
-    test_obj = Response(response)
-    test_obj.assert_status_code(200).validate(User)
+def test_get_user_list(get_users):
+    Response(get_users).assert_status_code(200).validate(User)
+    print(get_users)
